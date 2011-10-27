04cb9bb902dd0edd05bc190007417bfdb7a89d7e
FUKAUMI Naoki <naobsd@gmail.com>
2011-10-27 22:33:00 +0900

    set to panel mode if set_display_mode.sh is run w/o arg set_display_mode.sh is not needed for recovery

--- full_aml8726m.mk
+++ full_aml8726m.mk
@@ -34,10 +34,6 @@ PRODUCT_COPY_FILES += \
     device/amlogic/aml8726m/prebuilt/jogball.kl:system/usr/keylayout/jogball.kl
 
 PRODUCT_COPY_FILES += \
-    device/amlogic/aml8726m/prebuilt/fbset:recovery/root/sbin/fbset \
-    device/amlogic/aml8726m/prebuilt/set_display_mode.sh:recovery/root/sbin/set_display_mode.sh
-
-PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
