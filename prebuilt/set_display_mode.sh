43c3de246d8bc0036af5260941d487f7b03367cd
FUKAUMI Naoki <naobsd@gmail.com>
2011-10-28 15:06:01 +0900

    use default value for (unused) fb1

--- prebuilt/set_display_mode.sh
+++ prebuilt/set_display_mode.sh
@@ -4,25 +4,25 @@ case $1 in
     480p)
         /system/bin/fbset -fb /dev/graphics/fb0 -g 1280 768 1280 1536 16 -rgba 5/11,6/5,5/0,0/0
         echo $1 > /sys/class/display/mode
-        echo 0 0 1280 768 0 0 1280 768 > /sys/class/display/axis
+        echo 0 0 1280 768 0 0 32 32 > /sys/class/display/axis
     ;;
 
     720p)
         /system/bin/fbset -fb /dev/graphics/fb0 -g 1280 768 1280 1536 16 -rgba 5/11,6/5,5/0,0/0
         echo $1 > /sys/class/display/mode
-        echo 0 0 1280 768 0 0 1280 768 > /sys/class/display/axis
+        echo 0 0 1280 768 0 0 32 32 > /sys/class/display/axis
     ;;
 
     1080p)
         /system/bin/fbset -fb /dev/graphics/fb0 -g 1280 768 1280 1536 16 -rgba 5/11,6/5,5/0,0/0
         echo $1 > /sys/class/display/mode
-        echo 320 156 1280 768 320 156 1280 768 > /sys/class/display/axis
+        echo 320 156 1280 768 0 0 32 32 > /sys/class/display/axis
     ;;
 
     panel)
         /system/bin/fbset -fb /dev/graphics/fb0 -g 1280 768 1280 1536 16 -rgba 5/11,6/5,5/0,0/0
         echo $1 > /sys/class/display/mode
-        echo 0 0 1280 768 0 0 1280 768 > /sys/class/display/axis
+        echo 0 0 1280 768 0 0 32 32 > /sys/class/display/axis
     ;;
     
     *)
@@ -30,5 +30,5 @@ case $1 in
         echo "       Default to panel"
         /system/bin/fbset -fb /dev/graphics/fb0 -g 1280 768 1280 1536 16 -rgba 5/11,6/5,5/0,0/0
         echo panel > /sys/class/display/mode
-        echo 0 0 1280 768 0 0 1280 768 > /sys/class/display/axis
+        echo 0 0 1280 768 0 0 32 32 > /sys/class/display/axis
 esac
